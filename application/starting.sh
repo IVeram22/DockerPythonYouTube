e03cf48b5d42ab12433374e9bd65c6f2cc31628f
Ivan Veramyou <iveram@Ivans-MacBook-Air.local>
2020-05-12 15:12:50 +0300

    refactoring

--- application/starting.sh
+++ application/starting.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 echo "Docker Container 'web_docker_app' container was starting."
 echo "* Running on http://0.0.0.0:8080/ "
-docker run --rm --name web_docker_app -d -p 8080:8080 -v /Users/iveram/Documents/docker/application/Resources:/usr/src/app/Resources docker_app
-
+docker run --rm --name web_docker_app -d -p 8080:8080 -v 'Absolute Path':/usr/src/app/Resources docker_app
